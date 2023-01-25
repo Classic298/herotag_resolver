e2a50211aab42f5a1bd2533028bce80e8fc46e58
Classic <ja.gruber@outlook.com>
2023-01-25 14:42:50 +0100

    improved code

--- resolver.py
+++ resolver.py
@@ -1,23 +1,40 @@
-"""This code uses the open function to read the file located at the specified filepath and then
-uses the requests library to send a GET request to the MultiversX API for each herotag in the file,
-one by one. It extracts the "address" field from the JSON response to build a list of addresses."""
-import requests
+"""This code uses the threading library to create a new thread for each herotag to be resolved.
+Each thread runs the function resolve_herotag which takes the herotag and a list of addresses as
+arguments. The function uses the requests library to send a GET request to the Elrond API to
+resolve the herotag into an address. If the status code of the response is not 200, it means the
+herotag could not be resolved and it prints an informative message, otherwise, it adds the herotag
+and the resolved address to the list of addresses.
 
-def resolve_herotags(filepath):
+The main thread waits for all the worker threads to finish by calling join() method on each thread
+object before writing the results to the new file and returning the list of addresses."""
+import requests
+import threading
+
+def resolve_herotag(herotag, addresses):
+    url = f"https://api.multiversx.com/usernames/{herotag}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f'herotag {herotag} could not be resolved')
+        return
+    address = response.json()["address"]
+    addresses.append((herotag, address))
+
+def resolve_herotags(filepath, new_filepath):
     addresses = []
     with open(filepath, "r") as file:
+        threads = []
         for line in file:
             herotag = line.strip()
-            url = f"https://api.multiversx.com/usernames/{herotag}"
-            response = requests.get(url)
-            address = response.json()["address"]
-            addresses.append(address)
-    return addresses
-
+            t = threading.Thread(target=resolve_herotag, args=(herotag, addresses))
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
+    
     with open(new_filepath, "w") as new_file:
-            for address in addresses:
-                new_file.write(address + "\n")
-        return addresses
+        for herotag, address in addresses:
+            new_file.write(f"{address}\n")
+    return addresses
 
 filepath = "herotags.txt"
 new_filepath = "addresses.txt"
