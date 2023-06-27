43e3d0d96da811b5512d51b697cbf73bd4ef6701
Classic <ja.gruber@outlook.com>
2023-06-28 01:27:59 +0200

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,47 +1,46 @@
 import requests
-import threading
-import time
 
-def resolve_herotag(herotag, addresses):
+def resolve_herotag(herotags):
     # Resolves each herotag
-    url = f"https://api.multiversx.com/usernames/{herotag}"
-    response = requests.get(url)
-    # Checks, if response is not 200. If that's the case, herotag not found
-    if response.status_code != 200:
-        print(f'herotag {herotag} could not be resolved')
-        return
-    # Otherwise, get the address from the reponse and add it to the list of addresses
-    address = response.json()["address"]
-    addresses.append((herotag, address))
-
-def resolve_herotags(filepath, new_filepath):
-    addresses = []
-    # Opens the provided file in read only mode
-    with open(filepath, "r") as file:
-        threads = []
-        for line in file:
-            # API rate limit
-            time.sleep(1.2)
-            # Get next line
-            herotag = line.strip()
-            # Start thread and call the resolve function
-            t = threading.Thread(target=resolve_herotag, args=(herotag, addresses))
-            # Start thread
-            t.start()
-            threads.append(t)
-        for t in threads:
-            t.join()
+    url = "https://index.multiversx.com/accounts/_search"
+    body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "terms": {
+                            "userName.keyword": herotags
+                        }
+                    }
+                ]
+            }
+        },
+        "size": 10000
+    }
+    response = requests.post(url=url, json=body).json()
     
-    with open(new_filepath, "w") as new_file:
-        # Write back the resolved addresses in a new file
-        for herotag, address in addresses:
-            new_file.write(f"{address}\n")
-    # Also return the addresses
-    return addresses
+    # Iterates through all hits
+    hits = response["hits"]["hits"]
+    output = {}
 
-# Specifies input and output file
-filepath = "herotags.txt"
-new_filepath = "addresses.txt"
+    for hit in hits:
+        output[hit["_source"]["userName"]] = hit["_id"]
 
-# Calls the main function and prints the returned, resolved addresses in the end
-print(resolve_herotags(filepath, new_filepath))
+    return output
+
+# Reads the file herotags.txt and puts its content into a list
+with open("herotags.txt", "r") as f:
+    herotags = [line.strip().replace('@', '') if line.strip().endswith('.elrond') else line.strip().replace('@', '')+'.elrond' for line in f]
+
+# Calls the main function
+resolved = resolve_herotag(herotags)
+
+# Open the file for writing addresses
+with open("addresses.txt", "w") as f:
+    for herotag in herotags:
+        if herotag in resolved:
+            # Write the address into the file
+            f.write(resolved[herotag] + "\n")
+        else:
+            # Print the herotag that could not be resolved
+            print(f"Could not resolve herotag: {herotag}")
